cda9bb043ef207c03ac38c646795e826155a0c6d
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2020-12-19 08:12:42 +0000

    Bump develop version

--- api/index.php
+++ api/index.php
@@ -9,4 +9,38 @@
 // (at your option) any later version.
 //
 // The J4FCore library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the J4FCore library. If not, see <http://www.gnu.org/licenses/>.
+
+include('../CONFIG.php');
+include('../src/DB/DBTransactions.php');
+include('../src/DB/DBContracts.php');
+include('../src/DB/DBBlocks.php');
+include('../src/DB/DBBase.php');
+include('../src/DB/DB.php');
+include('../src/ColorsCLI.php');
+include('../src/Display.php');
+include('../src/Subprocess.php');
+include('../src/BootstrapNode.php');
+include('../src/ArgvParser.php');
+include('../src/Tools.php');
+include('../src/Wallet.php');
+include('../src/Block.php');
+include('../src/Blockchain.php');
+include('../src/Gossip.php');
+include('../src/Key.php');
+include('../src/Pki.php');
+include('../src/PoW.php');
+include('../src/REGEX.php');
+include('../src/Transaction.php');
+include('../src/GenesisBlock.php');
+include('../src/Peer.php');
+include('../src/Miner.php');
+include('../src/SmartContract.php');
+include('../src/SmartContractStateMachine.php');
+include('../src/J4FVM/J4FVMTools.php');
+incl
