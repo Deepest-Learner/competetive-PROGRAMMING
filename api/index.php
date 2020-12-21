2b51b13857367fc264525a96cb55fd9d7d0678ba
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2020-12-21 22:47:36 +0000

    add some css

--- api/index.php
+++ api/index.php
@@ -239,4 +239,32 @@ if ($id != null) {
                             );
                         } else if (strlen($wallet) == 0) {
                             $response_jsonrpc['error'] = array(
-                                'code
+                                'code'    => -32603,
+                                'message' => 'Internal error'
+                            );
+                        } else {
+                            //Write result on response
+                            $response_jsonrpc['result'] = Wallet::API_GetBalance($wallet,$isTestnet);
+                        }
+                    }
+                break;
+
+                case 'j4f_getPendingBalance':
+                    if (!isset($params['wallet']) || strlen($params['wallet']) == 0) {
+                        $response_jsonrpc['error'] = array(
+                            'code'    => -32602,
+                            'message' => 'Invalid params'
+                        );
+                    } else {
+
+                        //Check if wallet is a pubKey
+                        if (strlen($params['wallet']) > 59) {
+                            //Get wallet from Public key
+                            $wallet = Wallet::GetWalletAddressFromPubKey($params['wallet']);
+                        } else {
+                            $wallet = $params['wallet'];
+                        }
+
+                        //Check if have wallet
+                        if (strlen($wallet) < 59) {
+           
