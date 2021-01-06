602e8c28bd23d7d8bf7f9c44aa2bd29929375d12
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-01-06 17:38:25 +0000

    Create game.js

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -1,2 +1,36 @@
 <?php
-// This file is dynamically generated from js.l and js.y by metaphp'
+// This file is dynamically generated from js.l and js.y by metaphp's CFG parser
+// Do not waste your time editing it or reading it. Move along. Thank you.
+
+function __lambda_1 (&$type, &$text, $match, &$state, &$context) {
+$context++; $state="code";
+}
+function __lambda_2 (&$type, &$text, $match, &$state, &$context) {
+$context--; if ($context <= 0) $state = "rule";
+}
+function __lambda_3 (&$type, &$text, $match, &$state, &$context) {
+$text = substr($text,1);
+}
+function __lambda_4 (&$type, &$text, $match, &$state, &$context) {
+$text = substr($text,1,strlen($text)-2);
+}
+function __lambda_5 (&$type, &$text, $match, &$state, &$context) {
+$state = 'rule';
+}
+function __lambda_6 (&$type, &$text, $match, &$state, &$context) {
+$state = 'regex';
+}
+function __lambda_7 (&$type, &$text, $match, &$state, &$context) {
+if ($text=='ignore') $type='ignore';
+}
+function __lambda_8 (&$type, &$text, $match, &$state, &$context) {
+$context--; if ($context <= 0) $state = "INITIAL";
+}
+function __lambda_9 ($tokens) {
+return new preg_scanner_definition($tokens[0]);
+}
+function __lambda_10 ($tokens) {
+$tokens[0]->add_common_rule($tokens[1]); return $tokens[0];
+}
+function __lambda_11 ($tokens) {
+$tokens[0]->add_rule_list($tokens[1
