28af8fefa391430ae9e7df368b1de38081da97fc
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-02-19 22:55:25 +0000

    Fix line too long

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -1750,4 +1750,75 @@ $dpa = array (
       array (
         0 => 'fold',
         1 => 18,
-        2 => 's
+        2 => 's184',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'fold',
+        1 => 18,
+        2 => 's184',
+      ),
+      'T_DECIMAL' => 
+      array (
+        0 => 'fold',
+  
