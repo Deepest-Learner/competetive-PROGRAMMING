09b660eba3470504a43c0bd0fe995b427a3a51b5
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-03-09 08:42:04 +0000

    trabalhando

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -2857,4 +2857,63 @@ $dpa = array (
       ),
       'T_MINUSMINUS' => 
       array (
-        0
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'ExpressionStatement',
+        2 => 's435',
+      ),
+      'T_LEFTBRACKET' => 
+      array 
