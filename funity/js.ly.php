6ae3198c3c258065b8c80c5f9cdc6fa50c7f2f07
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-03-15 20:58:21 +0000

    Completed.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -3476,4 +3476,68 @@ $dpa = array (
       array (
         0 => 'push',
         1 => 'Statement',
-        2 => 's
+        2 => 's471',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's471',
+      ),
+      'T_WORD' => 
+      array (
+     
