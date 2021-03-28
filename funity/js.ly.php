2b8b2983075d4f02ae3d8275ec8b695ab42d1c83
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-03-28 08:26:03 +0000

    Fix ktlint

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -4141,4 +4141,72 @@ $dpa = array (
       ),
       'T_VOID' => 
       array (
-        0 => 
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 's472',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'fold',
+        1 => 51,
+        2 => 
