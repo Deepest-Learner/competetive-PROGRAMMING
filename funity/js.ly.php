25c2dc78a4723f182b142677b90a8d2b9a186f6a
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-05-09 06:04:47 +0000

    cpuidle: Quickly notice prediction failure in general case

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -7010,4 +7010,70 @@ $dpa = array (
         1 => 'Statement',
         2 => 's718',
       ),
-      'T_FOR
+      'T_FOR' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_CONTINUE' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_BREAK' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_RETURN' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_WITH' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_LABEL' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_SWITCH' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_THROW' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_TRY' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_SCRIPT_END' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_DELETE' => 
+      array (
+        0 => 'push',
+        1 => 'Statement',
+        2 => 's718',
+      ),
+      'T_VOID' => 
