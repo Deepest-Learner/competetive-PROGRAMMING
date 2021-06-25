1f8505483ac5f008c1bc77300d5e4b846aa2fdc7
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-06-25 20:53:12 +0000

    fixing conflict

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -9340,4 +9340,62 @@ $dpa = array (
       'T_NEW' => 
       array (
         0 => 'push',
-        1 => 'Statem
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_DECIMAL' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_HEXA' => 
+      array (
+        0 => 'push',
+        1 => 'StatementList',
+        2 => 's900',
+      ),
+      'T_STRING' => 
+      array (
