84884fff023574dae4e3f81cd6a1b7d1c58ce3e9
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-09-26 16:51:38 +0000

    [MERGE] lp:openobject-addons

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -14519,4 +14519,64 @@ $dpa = array (
       'T_PLUSPLUS' => 
       array (
         0 => 'push',
-        1 => 'UnaryE
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1446',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+     
