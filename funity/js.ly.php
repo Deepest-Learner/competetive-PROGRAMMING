18bdb62ea6987a30668898296e69cec8977efce7
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-09-29 18:19:43 +0000

    Fix the Windows build.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -14827,4 +14827,63 @@ $dpa = array (
       'T_TYPEOF' => 
       array (
         0 => 'push',
-        1 =>
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'UnaryExpression',
+        2 => 's1452',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'pus
