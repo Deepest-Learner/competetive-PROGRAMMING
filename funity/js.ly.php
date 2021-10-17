bce2274ca51e147dc3f37b042ad44b6a01bfe988
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-10-17 12:43:54 +0000

    writeback: fix occasional slow sync(1)

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -15839,4 +15839,62 @@ $dpa = array (
       array (
         0 => 'push',
         1 => 'AdditiveExpression',
-        2 => 's1515
+        2 => 's1515',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'AdditiveExpression',
+        2 => 's1515',
+      ),
+      'T_WORD' => 
+      array (
+  
