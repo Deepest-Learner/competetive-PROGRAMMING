10bb6842ecf0591627fd4043d3ae258ade09410b
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-10-27 18:37:50 +0000

    fixed bootstrap

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -16510,4 +16510,63 @@ $dpa = array (
         1 => 's1578',
       ),
     ),
-    's1573
+    's1573' => 
+    array (
+      'T_DELETE' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_VOID' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's1579',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        
