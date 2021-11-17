b98821f8092594172e677fee5f3bd4c6a91e7627
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-11-17 21:44:33 +0000

    mm: test PageSwapBacked in lumpy reclaim

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -17789,4 +17789,59 @@ $dpa = array (
         2 => 's1628',
       ),
       'T_TYPEOF' => 
-      
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
