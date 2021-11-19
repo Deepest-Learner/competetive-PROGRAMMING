4469c7c290b6914ffd5da6fb0a2401c84c4ed8e0
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-11-19 12:51:19 +0000

    Added LICENSE.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -17904,4 +17904,62 @@ $dpa = array (
       ),
       'T_STRING' => 
       array (
-        0 => 'push'
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1628',
+      ),
+    ),
+    's1625' => 
+    array (
+      'T_DELETE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_VOID' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1629',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExp
