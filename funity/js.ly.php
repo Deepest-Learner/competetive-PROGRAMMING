ea31c4da259b90276f5bf7f8391b07f7feb226d3
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-11-23 19:47:17 +0000

    Fix race condition.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -18079,4 +18079,62 @@ $dpa = array (
       'T_PLUS' => 
       array (
         0 => 'push',
-        1 => 'RelationalExpre
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's1630',
+      ),
+      'T_LEFTBRACKET' => 
+     
