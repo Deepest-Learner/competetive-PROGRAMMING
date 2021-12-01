3579a6b2615f2c8d8763a01e11e87a766cf1b544
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-12-01 14:24:38 +0000

    Nuevo post

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -18619,4 +18619,61 @@ $dpa = array (
         1 => 'EqualityExpression',
         2 => 's1648',
       ),
- 
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpression',
+        2 => 's1648',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressio
