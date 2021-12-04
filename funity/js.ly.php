ff81a180e9f4a6003ddb901178a1418f74e78bba
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2021-12-04 18:40:18 +0000

    block: Don't access request after it might be freed

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -18915,3 +18915,59 @@ $dpa = array (
       ),
       'T_VOID' => 
       array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'BitwiseANDExpression',
+        2 => 's1665',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+ 
