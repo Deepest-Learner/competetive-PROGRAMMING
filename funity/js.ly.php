aa7d3e592aa1ccc79bb646724251d4bdbc532a85
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-01-03 20:55:49 +0000

    posix_types.h: Cleanup stale __NFDBITS and related definitions

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -20905,4 +20905,60 @@ $dpa = array (
       array (
         0 => 'push',
         1 => 'AssignmentExpression',
-        2 => 's1778
+        2 => 's1778',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_LEFTBRACE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's1778',
+      ),
+      'T_DECIMAL' =
