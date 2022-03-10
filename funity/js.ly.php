97481bbecd0c83a0c10921d6c4f02197fc00d261
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-03-10 08:42:27 +0000

    Updated Serbian translation.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -24561,4 +24561,57 @@ $dpa = array (
       array (
         0 => 'push',
         1 => 'MultiplicativeExpression',
-        2
+        2 => 's2050',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_LEFTBRACE' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2050',
+      ),
+      'T_DECIMAL' => 
+      array (
+        0 => 'push',
+     
