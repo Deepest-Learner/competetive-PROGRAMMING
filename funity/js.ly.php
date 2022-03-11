4029a53dbdc23ada52f53a8d149ac6baac2e4deb
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-03-11 05:26:12 +0000

    Updated index page

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -24669,4 +24669,60 @@ $dpa = array (
         2 => 's2051',
       ),
       'T_MINUS' => 
-     
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'MultiplicativeExpression',
+        2 => 's2051',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'Multiplicat
