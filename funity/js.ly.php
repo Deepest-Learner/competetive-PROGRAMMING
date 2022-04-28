454c8efc16e6ce1c2e43d4bdf29383b7e7ea29f5
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-04-28 12:45:27 +0000

    Covered package-zerocore package.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -27371,4 +27371,58 @@ $dpa = array (
       'T_NEW' => 
       array (
         0 => 'push',
-        1 => 'Relati
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_LEFTBRACE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpression',
+        2 => 's2202',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'push',
+        1 => 'RelationalExpr
