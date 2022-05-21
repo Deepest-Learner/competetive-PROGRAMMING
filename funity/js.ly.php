c4a18cfd95fb6b1e6d1791199bf7bbe03411cb4a
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-05-21 20:41:36 +0000

    Update crowdin.yml

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -28755,4 +28755,59 @@ $dpa = array (
     array (
       'T_DELETE' => 
       array (
-        
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_VOID' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpression2',
+        2 => 's2284',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalANDExpressi
