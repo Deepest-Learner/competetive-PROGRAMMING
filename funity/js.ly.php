3eeaa815df2cd79b8ccfaee3822b67cea70a2765
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-05-28 12:35:09 +0000

    updated login page

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -29049,4 +29049,59 @@ $dpa = array (
     array (
       'T_DELETE' => 
       array (
-  
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_VOID' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
+        2 => 's2305',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'LogicalORExpression2',
