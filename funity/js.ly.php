6df5f69702275536e9258e91aa769b7487145945
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-06-03 12:44:09 +0000

    Few changes.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -29400,4 +29400,59 @@ $dpa = array (
       ),
       'T_FUNCTION' => 
       array (
- 
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_LEFTBRACE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_TRUE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_FALSE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_DECIMAL' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_HEXA' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression',
+        2 => 's2309',
+      ),
+      'T_STRING' => 
+      array (
+        0 => 'push',
+        1 => 'Assignment
