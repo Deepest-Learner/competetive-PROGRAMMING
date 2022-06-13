97cf7ca87307ffa2e6bd63ea97a8329951429f1c
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-06-13 10:49:58 +0000

    Bump to version 2.0.1

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -29924,4 +29924,60 @@ $dpa = array (
       array (
         0 => 'push',
         1 => 'AssignmentExpression2',
-        2 =>
+        2 => 's2349',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'AssignmentExpression2',
+        2 => 's2349',
+      ),
+      'T_LEFTBRACKET' =>
