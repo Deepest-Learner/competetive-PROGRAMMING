eb1d09989b45ff047ed3e00e6dc902f2932bfc23
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-07-09 17:09:44 +0000

    Added colors

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -30936,4 +30936,65 @@ $dpa = array (
         0 => 'push',
         1 => 'ShiftExpression',
         2 => 's2409',
- 
+      ),
+      'T_VOID' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_TYPEOF' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'ShiftExpression',
+        2 => 's2409',
+      ),
+     
