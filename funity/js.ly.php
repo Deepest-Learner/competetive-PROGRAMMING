ea82e497afdc298df547d7e5a457611cfa7514e7
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-07-23 22:02:38 +0000

    Removed debug log.

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -32129,4 +32129,61 @@ $dpa = array (
       'T_TYPEOF' => 
       array (
         0 => 'push',
-        1 => 'EqualityExp
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_PLUSPLUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_MINUSMINUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_PLUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_MINUS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'EqualityExpressionNoIn',
+        2 => 's2470',
+      ),
+   
