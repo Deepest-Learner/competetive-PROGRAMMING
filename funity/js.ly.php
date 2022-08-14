d228ca82980dac45f235691a133c7a31cbf0c42e
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-08-14 20:43:03 +0000

    Updated status

--- funity/js.ly.php
+++ funity/js.ly.php
@@ -34117,4 +34117,58 @@ $dpa = array (
       'T_MINUS' => 
       array (
         0 => 'push',
-      
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_TILDE' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_BANG' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_NEW' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_THIS' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_LEFTPARENS' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_FUNCTION' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_WORD' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_NULL' => 
+      array (
+        0 => 'push',
+        1 => 'ConditionalExpressionNoIn',
+        2 => 's2585',
+      ),
+      'T_LEFTBRACKET' => 
+      array (
+        0 => 'push',
+ 
