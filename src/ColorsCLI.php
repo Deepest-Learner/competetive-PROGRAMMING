ff4c8eaf5e54b60c139f4b57d693e378be80ba67
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-09-29 15:21:59 +0000

    Added some docstrings

--- src/ColorsCLI.php
+++ src/ColorsCLI.php
@@ -13,4 +13,30 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser
+// You should have received a copy of the GNU Lesser General Public License
+// along with the J4FCore library. If not, see <http://www.gnu.org/licenses/>.
+
+class ColorsCLI {
+
+    public static $FG_BLACK         = "\033[0;30m";
+    public static $FG_DARK_GRAY     = "\033[1;30m";
+    public static $FG_RED           = "\033[0;31m";
+    public static $FG_LIGHT_RED     = "\033[1;31m";
+    public static $FG_GREEN         = "\033[0;32m";
+    public static $FG_LIGHT_GREEN   = "\033[1;32m";
+    public static $FG_BROWN         = "\033[0;33m";
+    public static $FG_YELLOW        = "\033[1;33m";
+    public static $FG_BLUE          = "\033[0;34m";
+    public static $FG_LIGHT_BLUE    = "\033[1;34m";
+    public static $FG_PURPLE        = "\033[0;35m";
+    public static $FG_LIGHT_PURPLE  = "\033[1;35m";
+    public static $FG_CYAN          = "\033[0;36m";
+    public static $FG_LIGHT_CYAN    = "\033[1;36m";
+    public static $FG_LIGHT_GRAY    = "\033[0;37m";
+    public static $FG_WHITE         = "\033[1;37m";
+
+    public static $BG_BLACK         = "\033[40m";
+    public static $BG_RED           = "\033[41m";
+    public static $BG_GREEN         = "\033[42m";
+    public static $BG_YELLOW        = "\033[43m";
+    public sta
