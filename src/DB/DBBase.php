d87c443781f0b7655d9ea1ea2b4def3d3cea632f
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-10-03 19:06:05 +0000

    Release v0.7.3

--- src/DB/DBBase.php
+++ src/DB/DBBase.php
@@ -1,3 +1,35 @@
 <?php
 // Copyright 2018 MaTaXeToS
-// Copyright 
+// Copyright 2019-2020 The Just4Fun Authors
+// This file is part of the J4FCore library.
+//
+// The J4FCore library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The J4FCore library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the J4FCore library. If not, see <http://www.gnu.org/licenses/>.
+
+class DBBase extends DBBlocks {
+
+    /**
+     * @param string $table
+     * @return bool
+     */
+    public function truncate(string $table) : bool {
+        if ($this->db->query("TRUNCATE TABLE " . $table.";"))
+            return true;
+        return false;
+    }
+
+	/**
+     * Check if have RocksDB Engine installed on MariaDB Server
+     *
+     * @return bool`
+     
