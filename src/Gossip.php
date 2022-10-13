1f066a17941da2381e827c084ef8ca856042f34f
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-10-13 17:08:07 +0000

    Regenerate gemspec for version 0.10.0

--- src/Gossip.php
+++ src/Gossip.php
@@ -5,4 +5,50 @@
 //
 // The J4FCore library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the Lic
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The J4FCore library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the J4FCore library. If not, see <http://www.gnu.org/licenses/>.
+
+use React\Socket\ConnectionInterface;
+
+final class Gossip {
+
+    public $name;
+    public $key;
+    public $port;
+    public $ip;
+    public $enable_mine;
+    public $pending_transactions;
+    public $coinbase;
+    public $syncing;
+    public $config;
+    public $peers = [];
+    public $difficulty;
+    public $isTestNet;
+
+    /** @var DB $chaindata */
+    public $chaindata;
+    private $make_genesis;
+    private $bootstrap_node;
+
+    private $loop_x5 = 0;
+	private $loop_x10 = 0;
+	private $loop_x15 = 0;
+
+	public $isBusy = false;
+
+    /**
+     * Gossip constructor
+     *
+     * @param DB        $db
+     * @param string    $name
+     * @param string    $ip
+     * @param string    $port
+     * 
