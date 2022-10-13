51515143d56558114f313bca0e415c1507bf74de
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2022-10-13 20:00:50 +0000

    [l10n] Updated Italian translation.

--- src/Gossip.php
+++ src/Gossip.php
@@ -51,4 +51,36 @@ final class Gossip {
      * @param string    $name
      * @param string    $ip
      * @param string    $port
-     * 
+     * @param bool      $enable_mine
+     * @param bool      $make_genesis_block
+     * @param bool      $bootstrap_node
+     * @param bool      $isTestNet
+     */
+    public function __construct(DB $db,string $name,string $ip,string $port,bool $enable_mine,bool $make_genesis_block=false,bool $bootstrap_node = false,bool $isTestNet=false, int $sanityBlockchain=-1) {
+		//Clear screen
+		Display::ClearScreen();
+
+		//Init Display message
+		Display::print("Welcome to the %G%J4F node%W% - %G%Version%W%: " . VERSION);
+		Display::print("Maximum peer count                       %G%value%W%=".PEERS_MAX);
+		Display::print("PeerID %G%".Tools::GetIdFromIpAndPort($ip,$port));
+
+		$this->make_genesis = $make_genesis_block;
+		$this->bootstrap_node = $bootstrap_node;
+		$this->isTestNet = $isTestNet;
+		$this->enable_mine = $enable_mine;
+
+		//Save node info
+		$db->SetConfig('node_name',$name);
+		$db->SetConfig('node_ip',$ip);
+		$db->SetConfig('node_port',$port);
+		$db->SetConfig('node_version',VERSION);
+
+		//Set network config
+		if ($this->isTestNet)
+			$db->SetConfig('network','testnet');
+		else
+			$db->SetConfig('network','mainnet');
+
+		//Set miner config
+		if ($this->en
