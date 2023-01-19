49c2264592761c54d0e013fdad3d2e159ea86fe4
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2023-01-19 19:43:21 +0000

    Empty commit

--- src/Socket.php
+++ src/Socket.php
@@ -1,3 +1,36 @@
 <?php
 // Copyright 2018 MaTaXeToS
-// Copyright 2019-2020 The
+// Copyright 2019-2020 The Just4Fun Authors
+// This file is part of the J4FCore library.
+//
+// The J4FCore library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The J4FCore library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the J4FCore library. If not, see <http://www.gnu.org/licenses/>.
+
+use React\Socket\ConnectionInterface;
+
+class Socket {
+
+	/**
+     * Send message to socket
+     *
+     * @param string $ip
+     * @param string $port
+     * @param array $data
+	 * @return bool
+     */
+
+    public static function sendMessage(string $ip='127.0.0.1',string $port='6969', array $data = []) : bool {
+		if (!self::isAlive($ip,$port))
+			return false;
+
+		$loop = React\EventLoop
