36fbde4b80d315c2f113542a94eb155c07b74bd3
Deepest-Learner <111499701+Deepest-Learner@users.noreply.github.com>
2023-02-13 07:16:11 +0000

    0.3

--- src/Tools.php
+++ src/Tools.php
@@ -327,4 +327,51 @@ class Tools {
         $t2 = strtotime($dt2);
 
         $dtd = new stdClass();
-   
+        $dtd->interval = $t2 - $t1;
+        $dtd->total_sec = abs($t2-$t1);
+        $dtd->total_min = floor($dtd->total_sec/60);
+        $dtd->total_hour = floor($dtd->total_min/60);
+        $dtd->total_day = floor($dtd->total_hour/24);
+
+        $dtd->day = $dtd->total_day;
+        $dtd->hour = $dtd->total_hour -($dtd->total_day*24);
+        $dtd->min = $dtd->total_min -($dtd->total_hour*60);
+        $dtd->sec = $dtd->total_sec -($dtd->total_min*60);
+        return $dtd;
+    }
+
+	/**
+	 * Get age date
+	 *
+	 * @param $ageObject
+	 * @return string
+	 */
+	public static function getAge($ageObject) {
+		$ageBlockMessage = "";
+
+		if ($ageObject->day > 1)
+			$ageBlockMessage .= $ageObject->day." days ";
+		elseif ($ageObject->day > 0)
+				$ageBlockMessage .= $ageObject->day." day ";
+
+		if ($ageObject->hour > 0)
+			$ageBlockMessage .= $ageObject->hour." hrs ";
+
+		if ($ageObject->min > 0) {
+			if (strlen($ageBlockMessage) > 0)
+				$ageBlockMessage .= " ";
+
+			$ageBlockMessage .= $ageObject->min." mins ";
+		}
+
+		if ($ageObject->sec > 0) {
+			if (strlen($ageBlockMessage) > 0)
+				$ageBlockMessage .= " ";
+
+			$ageBlockMessage .= $ageObject->sec." secs";
+		}
+		return $ageBlockMessage;
+	}
+
+    /**
+     * Get global time usin
